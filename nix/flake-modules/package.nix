907377f8dc7c185f68d3b4b62a56e3b1edca4906
Jakob Lichterfeld <jakob-lichterfeld@gmx.de>
2025-07-19 11:17:32 +0200

    fix(nix): update mix dependency hash in nix builds

--- nix/flake-modules/package.nix
+++ nix/flake-modules/package.nix
@@ -18,7 +18,7 @@
         TOP_SRC = src;
         pname = "${pname}-mix-deps";
         inherit src version;
-        hash = "sha256-fdGbT7WpLnnGaoPkOuh17KrN6gMsc/Yqx+ALvNcCtns="; # if you change the mix deps, you need to update this hash
+        hash = "sha256-hVPlqkd6ozHi0WyGcpUpIyWtRuHfZJWkEB+uR5KOMHs="; # if you change the mix deps, you need to update this hash
         # hash = pkgs.lib.fakeHash;
       };
 
